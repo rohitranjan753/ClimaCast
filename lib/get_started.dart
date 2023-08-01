6e5a2e3666f805d16538c0fd8877f02a77a7042c
Rohit Ranjan <rohitgns54321@gmail.com>
2023-08-01 09:09:09 +0530

    dependency added

---
+++ lib/get_started.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatefulWidget {
+  const GetStarted({Key? key}) : super(key: key);
+
+  @override
+  State<GetStarted> createState() => _GetStartedState();
+}
+
+class _GetStartedState extends State<GetStarted> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather App'),
+      ),
+    );
+  }
+}
